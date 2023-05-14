36cd3b794267b0f9b4fb3bb606314626cb988771
Loe Lindström <lht.lindstrom@gmail.com>
2023-05-14 09:52:40 +0200

    updated deploy script

--- build-scripts/deploy-to-gh-pages.sh
+++ build-scripts/deploy-to-gh-pages.sh
@@ -22,6 +22,6 @@ git commit -m 'deploying'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:tecamo/camera.git main:gh-pages
+git push -f git@github.com:tecamo/camera.git master:gh-pages
 
 cd -
